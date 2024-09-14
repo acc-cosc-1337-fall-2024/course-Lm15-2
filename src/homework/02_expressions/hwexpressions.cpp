e391d5c4336cb93309f0b8e1217cd0680921eaf1
Lm15-2 <liam.mcdaniel@g.austincc.edu>
2024-09-14 00:01:36 +0000

    HW2

--- src/homework/02_expressions/hwexpressions.cpp
+++ src/homework/02_expressions/hwexpressions.cpp
@@ -1,5 +1,5 @@
 #include "hwexpressions.h"
-
+const double tax_rate = 0.0675;
 //example
 int add_numbers(int num1, int num2)
 {
@@ -8,6 +8,13 @@ int add_numbers(int num1, int num2)
 
 //write function code here
 
+double get_sales_tax_amount (double meal_amount){
+	
+	double product = meal_amount * tax_rate;
+	return product; 
+}
+double get_tip_amount(double meal_amount, double tip_rate){
 
-
-
+	double result = meal_amount * tip_rate;
+	return result; 
+}
