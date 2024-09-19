a77ece4646775ea39b3f967ef48b75d37e09443e
Lm15-2 <liam.mcdaniel@g.austincc.edu>
2024-09-19 17:22:44 +0000

    HW3

--- src/homework/03_decisions/decisions.cpp
+++ src/homework/03_decisions/decisions.cpp
@@ -1,4 +1,62 @@
-//write include statement for decisions header
+// write include statement for decisions header
+#include <iostream>
+#include "decisions.h"
 
+using std::cin;
+using std::cout;
+// Write code for function(s) code here
+string get_letter_grade_using_if(int grade)
+{
+  string letter;
+  if (grade >= 0 && grade <= 59)
+  {
+    letter = 'F';
+  }
+  else if (grade >= 60 && grade <= 69)
+  {
+    letter = 'D';
+  }
+  else if (grade >= 70 && grade <= 79)
+  {
+    letter = 'C';
+  }
+  else if (grade >= 80 && grade <= 89)
+  {
+    letter = 'B';
+  }
+  else if (grade >= 90 && grade <= 100)
+  {
+    letter = 'A';
+  }
+  else
+  {
+    letter = "value is not in the range of 000-100";
+    return letter;
+  }
+}
+string get_letter_grade_using_switch(int grade)
+{
 
-//Write code for function(s) code here
+  string letter;
+  switch (grade)
+  {
+  case 0 ... 59:
+    letter = 'F';
+    break;
+  case 60 ... 69:
+    letter = 'D';
+    break;
+  case 70 ... 79:
+    letter = 'C';
+    break;
+  case 80 ... 89:
+    letter = 'B';
+    break;
+  case 90 ... 100:
+    letter = 'A';
+    break;
+  default:
+    break;
+  }
+  return letter;
+}
--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -1,6 +1,37 @@
-//write include statements
+// write include statements
+#include <iostream>
+#include <decisions.h>
+using std::cin;
+using std::cout;
 
-int main() 
+int main()
 {
+	int choice;
+	int selection_if;
+	int selection_switch;
+
+	cout << "Please enter your selection for using if or switch to detrime letter grade from numeric:" << "\n";
+	cout << "1- Option 1(Using If)" << "\n";
+	cout << "2- Option 2(Using Switch)" << "\n";
+	cout << "3- Option 3(End Program)" << "\n";
+	cin >> choice;
+	switch (choice)
+	{
+	case 1:
+		cout << "You've chosen Option 1(Using If) \n";
+		cout << "Please enter the number grade: \n";
+		cin >> selection_if;
+		cout << "Your grade is: " << get_letter_grade_using_if(selection_if) << "\n";
+		break;
+	case 2:
+		cout << "You've chosen Option 2(Using Switch) \n";
+		cout << "Please enter the number grade: \n";
+		cin >> selection_switch;
+		cout << "your grade is: " << get_letter_grade_using_if(selection_switch) << "\n";
+		break;
+	case 3:
+		break;
+	}
+
 	return 0;
 }
