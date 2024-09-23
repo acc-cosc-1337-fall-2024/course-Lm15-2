95feb216db95e7b4cd3f549bfabf2efa456bcfcd
Lm15-2 <liam.mcdaniel@g.austincc.edu>
2024-09-23 22:56:26 +0000

    HW 3(Fixed)

--- src/homework/03_decisions/decisions.cpp
+++ src/homework/03_decisions/decisions.cpp
@@ -10,29 +10,30 @@ string get_letter_grade_using_if(int grade)
   string letter;
   if (grade >= 0 && grade <= 59)
   {
-    letter = 'F';
+    letter = "F";
   }
   else if (grade >= 60 && grade <= 69)
   {
-    letter = 'D';
+    letter = "D";
   }
   else if (grade >= 70 && grade <= 79)
   {
-    letter = 'C';
+    letter = "C";
   }
   else if (grade >= 80 && grade <= 89)
   {
-    letter = 'B';
+    letter = "B";
   }
   else if (grade >= 90 && grade <= 100)
   {
-    letter = 'A';
+    letter = "A";
   }
   else
   {
     letter = "value is not in the range of 000-100";
-    return letter;
+  
   }
+  return letter;
 }
 string get_letter_grade_using_switch(int grade)
 {
