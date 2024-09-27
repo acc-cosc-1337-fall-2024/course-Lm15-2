7d32e81da9cea2d131be6f0c3ad6a44563e82316
Lm15-2 <liam.mcdaniel@g.austincc.edu>
2024-09-27 22:55:35 +0000

    Hw4

--- src/homework/04_repetition/main.cpp
+++ src/homework/04_repetition/main.cpp
@@ -1,8 +1,38 @@
 //write include statements
-
+#include<iostream>
+#include "repetition.h"
+using namespace ::std;
 //write using statements
 
 int main() 
 {
-	return 0;
+	int choice;
+	int selectionFACTORIAL, selectionGCD1, selectionGCD2;
+	do{
+		cout << "Please enter your selection for using factorial or GCD: \n";
+	cout << "1- Option 1(Using factorial)\n";
+	cout << "2- Option 2(Using GCD)\n";
+	cout << "3- Option 3(End Program)\n";
+cin >> choice;
+switch (choice)
+	{
+	case 1:
+		cout << "You've chosen Option 1(Using factorial) \n";
+		cout << "Please enter the number: \n";
+		cin >> selectionFACTORIAL;
+		cout << "The Factorial of " << selectionFACTORIAL << " is: " << factorial(selectionFACTORIAL) << "\n";
+		break;
+	case 2:
+		cout << "You've chosen Option 2(Using GCD) \n";
+		cout << "Please enter the first number: \n";
+		cin >> selectionGCD1;
+		cout << "Please enter the second number: ";
+		cin >> selectionGCD2;
+		cout << "The gcd of "<< selectionGCD1 << "and " << selectionGCD2 <<" is: " << gcd(selectionGCD1, selectionGCD2) << "\n";
+		break;
+	}
+
+}
+while (choice != 3 );
+return 0;
 }
--- src/homework/04_repetition/repetition.cpp
+++ src/homework/04_repetition/repetition.cpp
@@ -1,3 +1,28 @@
-//add include statements
+// add include statements
+#include "repetition.h"
+using namespace ::std;
+// add function(s) code here
+int factorial(int num)
+{
 
-//add function(s) code here
+    int result = 1;
+    for (int i = 2; i <= num; i++)
+    {
+        result *= i;
+    }
+    return result;
+}
+
+int gcd(int num1, int num2)
+{
+    int result = min(num1, num2);
+    while (result > 0)
+    {
+        if (num1 % result == 0 && num2 % result == 0)
+        {
+            break;
+        }
+        result--;
+    }
+      return result; 
+} 
