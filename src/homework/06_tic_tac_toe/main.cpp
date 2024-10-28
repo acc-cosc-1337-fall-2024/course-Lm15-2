946eaae1692e980c88e470a213bf05c356f8ea08
Lm15-2 <liam.mcdaniel@g.austincc.edu>
2024-10-28 04:48:17 +0000

    tic tac toe

--- src/homework/06_tic_tac_toe/main.cpp
+++ src/homework/06_tic_tac_toe/main.cpp
@@ -1,6 +1,29 @@
-
+#include "tic_tac_toe.h"
 int main() 
 {
+	TicTacToe game;
+
+	string first_player;
+	char choice = 'y';
+	do{
+		cout<<"Enter first player: ";
+		cin>>first_player;
+
+		game.start_game(first_player);
+
+		int position;
+
+		while(!game.game_over()){
+			cout << "Enter a position: ";
+			cin>>position;
+			game.mark_board(position);
+			game.display_board();
+		}
+
+	cout<<"Enter y or Y to play again ";
+	cin>>choice;
+
+	}while(choice == 'y' || choice == 'Y');
 
 	return 0;
 }
